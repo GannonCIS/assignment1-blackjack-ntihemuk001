f2c75e22f42456e5b874cb0e42a492e2b75dc995
Jesse Ntihemuka <[ntihemuk001@gannon.edu]>
2020-01-27 17:54:25 -0500

    Create Hand Structure

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -10,5 +10,29 @@ package blackjack;
  * @author gubotdev
  */
 public class Hand {
+    Card[] myCards = new Card[5];
+    private int numOfCards = 0;
+    private int score = 0;
+    
+    public Hand(){
+        
+    }
+    
+    public int getNumofCards(){
+        return numOfCards; //gets the number of cards
+    }
+    
+    public int getScore(){
+        return score;
+    }
+    
+    public void addCard(Card newCard){
+        
+    }
+    
+    public void printHand(){
+        
+    }
+    
     
 }
