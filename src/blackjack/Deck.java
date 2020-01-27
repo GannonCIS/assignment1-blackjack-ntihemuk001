8542827009c0e7de359bead5b8bc8a47f159df75
Jesse Ntihemuka <[ntihemuk001@gannon.edu]>
2020-01-27 17:28:51 -0500

    Complete deck class structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,5 +10,33 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
+    private Card[] myCards = new Card[52];
+    private String[] ranks = {};
+    private String[] suits = {};
+    private int nextCard = 0;
     
+    public Deck(){ //this is a constructor//
+        
+    }
+    
+    private void initDeck(){
+        
+    }//from UML diagram
+    
+    private void shuffle(){
+        
+    }
+    
+    
+    public void printDeck(){ //printDeck is for testing only --will delete soon
+        
+    }
+    
+    public Card dealCard(){ //a class is A DATATYBE FOR AN OBJECT
+        
+        return null; //do not forget to change this!!!!!!!!!!!!!!!!!!!!!!
+        
+    } 
+    
+
 }
